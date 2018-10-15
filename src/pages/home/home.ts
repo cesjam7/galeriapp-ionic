c9bbba92b9110c39153f6dcc36a79b2d691424e9
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2018-10-14 23:13:09 -0500

    take photo and delete

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,54 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import {Camera, CameraOptions} from '@ionic-native/camera';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    public photos : any;
+    public base64Image : string;
+    constructor(public navCtrl: NavController, private camera : Camera, private alertCtrl : AlertController) {
 
-  constructor(public navCtrl: NavController) {
+    }
 
-  }
+    takePhoto() {
+        const options : CameraOptions = {
+            quality: 50, // picture quality
+            destinationType: this.camera.DestinationType.DATA_URL,
+            encodingType: this.camera.EncodingType.JPEG,
+            mediaType: this.camera.MediaType.PICTURE
+        }
+        this.camera.getPicture(options) .then((imageData) => {
+            this.base64Image = "data:image/jpeg;base64," + imageData;
+            this.photos.push(this.base64Image);
+            this.photos.reverse();
+        }, (err) => {
+            console.log(err);
+        });
+    }
+
+    deletePhoto(index) {
+        let confirm = this.alertCtrl.create({
+            title: 'Sure you want to delete this photo? There is NO undo!',
+            message: '',
+            buttons: [
+                {
+                    text: 'No',
+                    handler: () => {
+                        console.log('Disagree clicked');
+                    }
+                }, {
+                    text: 'Yes',
+                    handler: () => {
+                        console.log('Agree clicked');
+                        this.photos.splice(index, 1);
+                    }
+                }
+            ]
+        });
+        confirm.present();
+    }
 
 }
